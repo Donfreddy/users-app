1a8942f67ead1408c8b77119512ba906ece6db22
Donfreddy <freddytamwo@gmail.com>
2022-02-22 08:29:20 +0100

    add broadcast receiver network

--- app/src/main/java/com/example/usersapp/MainActivity.kt
+++ app/src/main/java/com/example/usersapp/MainActivity.kt
@@ -1,26 +1,55 @@
 package com.example.usersapp
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
+import android.net.ConnectivityManager
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.usersapp.network.Resource
+import com.google.android.material.snackbar.Snackbar
 import com.sevengps.sdn_mobile_app.repository.Repository
 
 
 class MainActivity : AppCompatActivity() {
   private lateinit var viewModel: MainViewModel
+  private lateinit var constraintLayout: ConstraintLayout
   private lateinit var userRecyclerView: RecyclerView
+
+  private var broadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+      val notConnected = intent.getBooleanExtra(
+        ConnectivityManager
+          .EXTRA_NO_CONNECTIVITY, false
+      )
+      if (notConnected) {
+        showSnackBar("Internet Disconnected. 👎", Color.RED)
+      } else {
+        showSnackBar("Internet Connected. 👍", Color.GREEN)
+      }
+    }
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
+    //
+    registerNetworkBroadcast()
+
     userRecyclerView = findViewById(R.id.user_lists)
+    constraintLayout = findViewById(R.id.root)
+
 
     val factory = MainViewModel.Factory(Repository())
     viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java];
@@ -36,6 +65,10 @@ class MainActivity : AppCompatActivity() {
             val adapter = UserAdapter(result)
             userRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
             userRecyclerView.adapter = adapter
+//            val userCardView: CardView = userRecyclerView.findViewById(R.id.cv_user)
+//            userCardView.setOnClickListener{
+//              Toast.makeText(this, "View User Detail", Toast.LENGTH_LONG).show()
+//            }
 
           }
         }
@@ -47,5 +80,22 @@ class MainActivity : AppCompatActivity() {
         }
       }
     })
+  }
+
+  private fun registerNetworkBroadcast() {
+    val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+    registerReceiver(broadcastReceiver, filter)
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    unregisterReceiver(broadcastReceiver)
+  }
+
+  fun showSnackBar(message: String, color: Int) {
+    val snackBar: Snackbar = Snackbar.make(constraintLayout, message, Snackbar.LENGTH_LONG)
+    snackBar.setBackgroundTint(ContextCompat.getColor(applicationContext, color))
+    snackBar.setActionTextColor(Color.WHITE)
+    snackBar.show()
   }
 }
